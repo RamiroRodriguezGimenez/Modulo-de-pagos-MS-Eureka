ea1c09000181b16ac5f570f9e66573b1add773ba
RamiroRodriguezGimenez <96449408+RamiroRodriguezGimenez@users.noreply.github.com>
2023-03-09 18:19:41 -0300

    Add files via upload

---
+++ eureka-server/src/main/java/com/paymentmodul/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.paymentmodul.eurekaserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableAdminServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
